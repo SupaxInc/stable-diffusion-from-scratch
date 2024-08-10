8ab6a9721efac69f26e06a60796fbd41fae1f71a
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-08-10 14:34:43 -0400

    Finished clip embeddings

--- model/clip.py
+++ model/clip.py
@@ -3,20 +3,56 @@ import torch.nn as nn
 import torch.nn.functional as F
 from attention import SelfAttention
 
+class CLIPEmbedding(nn.Module):
+    def __init__(self, vocab_size: int, embed_dim: int, max_position_embeddings: int):
+        super().__init__()
+        """
+        Args:
+            vocab_size: The size of the vocabulary. This determines the number of unique tokens
+                        that can be represented.
+
+            embed_dim: The dimensionality of the token embeddings. This is the size of the vector
+                        that represents each token.
+
+            max_position_embeddings: The maximum number of positions that can be embedded. This 
+                                     limits the maximum length of the input sequence.
+
+        The embeddings token, and position work together to provide a representation of the input text,
+        capturing both semantic meaning and positional information.
+        """
+        # Convert each token to a dense vector representation
+        self.token_embedding = nn.Embedding(vocab_size, embed_dim)
+        # Encode the position of each token in the sequence.
+        self.position_embedding = nn.Parameter(torch.zeros(max_position_embeddings, embed_dim))
+
+    def forward(self, tokens: torch.LongTensor) -> torch.FloatTensor:
+        """
+        Args:
+            tokens: Input tensor of token indices (Batch, Seq_Len).
+
+        Returns:
+            torch.FloatTensor: Embedded representation of the input tokens (Batch, Seq_Len, Dim).
+        """
+
+        # (Batch, Seq_Len) -> (Batch, Seq_Len, Dim)
+        x = self.token_embedding(tokens)
+
+        # Add the positional encoding to each embedding (it is not a fixed sinusoidal function but are learnt params from the model)
+        # Helps add more comple positional relationships in the input sequences for CLIP
+        x += self.position_embedding
+
+        # (Batch, Seq_Len, Dim)
+        return x
+
 """
     Look at the Self Attention header of StableDiffusion.md, we will build CLIP similar to the transformer image on the
     left hand side of the Self Attention picture. It begins with Positional Encoding that tells us the position of each token in a sequence
     and it is made up of multiple layers of Attention and Feed Forwards that go one after another.
 """
-
 class CLIP(nn.Module):
     def __init__(self):
         super().__init__()
-        # 1. CLIP embeddings convert tokens (which are already numbers) into dense vector representations
-        # 2. Each token is represented by a vector of size 768 (compared to 512 in original transformers)
-        # 3. The embedding layer combines token embeddings and position embeddings
-        # 4. 49408 is the vocabulary size, 768 is the embedding dimension, and 77 is the maximum sequence length
-        # 5. The resulting embeddings capture both semantic meaning and positional information of tokens
+        # The resulting embeddings capture both semantic meaning and positional information of tokens
         # TODO: Move these values to a config yaml file in the future
         self.embeddings = CLIPEmbedding(vocab_size=49408, embed_dim=768, max_position_embeddings=77)
 
@@ -30,12 +66,12 @@ class CLIP(nn.Module):
     def forward(self, tokens: torch.LongTensor) -> torch.FloatTensor:
         """
         Args:
-            tokens: Input tensor of token indices. Shape: (Batch, Seq_Len).
+            tokens: Input tensor of token indices (Batch, Seq_Len).
                     Uses LongTensor because CLIP encoders typically work with integer token indices,
                     where each index corresponds to a word or subword in the vocabulary.
         
         Returns:
-            torch.FloatTensor: Encoded representation of the input tokens. Shape: (Batch, Seq_Len, Dim).
+            torch.FloatTensor: Encoded representation of the input tokens (Batch, Seq_Len, Dim).
         """
         tokens = tokens.type(torch.long)
 
@@ -43,7 +79,6 @@ class CLIP(nn.Module):
         # (Batch, Seq_Len) -> (Batch, Seq_Len, Dim)
         state = self.embeddings(tokens)
 
-        # Apply multiple layers of self-attention and feed-forward networks
         for layer in self.layers:
             state = layer(state)
         
