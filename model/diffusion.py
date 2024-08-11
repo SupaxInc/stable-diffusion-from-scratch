3efdbbc211549348884d68a26b937dbdc301c382
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-08-10 23:44:59 -0400

    Added time embedding class used in diffusion model

--- model/diffusion.py
+++ model/diffusion.py
@@ -3,8 +3,36 @@ import torch.nn as nn
 import torch.nn.functional as F
 from attention import SelfAttention, CrossAttention
 
+class TimeEmbedding(nn.Module):
+    def __init__(self, embed_dim: int):
+        super().__init__()
+
+        # Feed forward network
+        self.linear_1 = nn.Linear(embed_dim, 4 * embed_dim)
+        self.linear_2 = nn.Linear(4 * embed_dim, 4 * embed_dim) # Larger scaling factor than CLIP encoder to capture more complex temporal info
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            x: Input time step tensor (1, 320).
+        
+        Returns:
+            torch.Tensor: Timestep tensor converted to a time embedding (1, 1280)
+        """
+
+        x = self.linear_1(x)
+
+        x = F.silu(x)
+
+        x = self.linear_2(x)
+        
+        # (1, 1280)
+        return x
+
 class Diffusion(nn.Module):
     def __init__(self):
+        super().__init__()
+
         # Used to understand the timestep that was used to create the noisy image
         self.time_embedding = TimeEmbedding(320)
 
