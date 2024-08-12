e8ab3d0f27832b2f6c8e4761da4df36bb7136f29
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-08-11 22:58:51 -0400

    CrossAttention block WIP and moving attention file to model folder

--- model/attention.py
+++ model/attention.py
@@ -27,8 +27,10 @@ class SelfAttention(nn.Module):
         # Number of attention heads for focusing on different parts of the input
         # This represents the multiple heads split from the vectors (Q1, Q2... K1, K2.. V1, V2, etc)
         self.n_heads = n_heads
-        # Dimension of each attention head (d_k in the formula of each head after ebing split from vectors)
-        self.d_head = d_embed // n_heads 
+        # Dimension of each attention head (d_k in the formula of each head after being split from vectors)
+        self.d_head = d_embed // n_heads
+
+        assert d_embed % n_heads == 0, "d_embed must be divisible by n_heads"
 
     def forward(self, x: torch.Tensor, causal_mask=False) -> torch.Tensor:
         """
@@ -54,7 +56,8 @@ class SelfAttention(nn.Module):
         input_shape = x.shape
         batch_size, sequence_length, d_embed = input_shape
 
-        intermediate_shape = (batch_size, sequence_length, self.n_heads, self.d_head)
+        # New shape to transform the query, key, and values
+        iterim_shape = (batch_size, sequence_length, self.n_heads, self.d_head)
 
         # 1: Project input to Q', K', V' vectors, essentially multiplying input tensor with combined WQ, WV, WK matrices
             # (Batch, Seq_Len, Dim) -> (Batch, Seq_Len, Dim * 3) -> chunk to 3 tensors of shape (Batch, Seq_Len, Dim)
@@ -62,9 +65,9 @@ class SelfAttention(nn.Module):
 
         # 2: Split the vectors into the number of heads Q1, Q2... K1, K2.. V1, V2, etc
             # Each (Batch, Seq_Len, Dim) -> (Batch, Seq_Len, Num Heads, Dim / Num Heads) -> (Batch, Num Heads, Seq_Len, Dim / Num Heads)
-        q = q.view(intermediate_shape).transpose(1, 2)
-        k = k.view(intermediate_shape).transpose(1, 2)
-        v = v.view(intermediate_shape).transpose(1, 2)
+        q = q.view(iterim_shape).transpose(1, 2)
+        k = k.view(iterim_shape).transpose(1, 2)
+        v = v.view(iterim_shape).transpose(1, 2)
 
         # Now work on computing the attention using the formula: Attention(Q, K, V) = softmax((Q * KT) / √dk) * V
 
@@ -116,3 +119,58 @@ class SelfAttention(nn.Module):
 
         # (Batch, Seq_Len, Dim (d_embed))
         return output
+
+class CrossAttention(nn.Module):
+    def __init__(self, n_heads: int, d_embed: int, d_cross: int, in_proj_bias=True, out_proj_bias=True):
+        """
+        This module implements cross-attention, allowing the model to attend to a different input
+        (context) than the one being transformed. It's crucial for incorporating text embeddings
+        into the image generation process.
+
+        Args:
+            n_heads: Number of attention heads.
+            d_embed: Dimensionality of the input embeddings (query).
+            d_cross: Dimensionality of the cross-attention input (key/value).
+            in_proj_bias: Whether to include bias in input projections. Defaults to True.
+            out_proj_bias: Whether to include bias in output projection. Defaults to True.
+        """
+        super().__init__()
+        
+        # Separate projection layers for query, key, and value instead of 1 big matrix combining all
+        self.q_proj = nn.Linear(d_embed, d_embed, bias=in_proj_bias)
+        self.k_proj = nn.Linear(d_cross, d_embed, bias=in_proj_bias)
+        self.v_proj = nn.Linear(d_cross, d_embed, bias=in_proj_bias)
+
+        # Output projection (WO matrix)
+        self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
+
+        # Number of attention heads for focusing on different parts of the input
+        # This represents the multiple heads split from the vectors (Q1, Q2... K1, K2.. V1, V2, etc)
+        self.n_heads = n_heads
+        # Dimension of each attention head (d_k in the formula of each head after being split from vectors)
+        self.d_head = d_embed // n_heads
+
+        assert d_embed % n_heads == 0, "d_embed must be divisible by n_heads"
+    
+    def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        """
+        In cross attention, the latent is the query and the key/value pair is the CLIP Embedding.
+
+        Args:
+            x: Latent of the noisy image (Batch, Seq_Len_Q, Dim_Q).
+            y: Context tensor of CLIPEmbedding (Batch, Seq_Len_KV, Dim_KV) = (Batch, 77, 768)
+        """
+
+        input_shape = x.shape
+        batch, seq_len, d_embed = input_shape
+
+        # New shape to transform the query, key, and values
+        iterim_shape = (batch, -1, self.n_heads, self.d_head)
+
+        # Multiply query by WQ matrix using the latent
+        q = self.q_proj(x)
+        # Multiply k/v with WK/WV matrices using the context 
+        k = self.k_proj(y)
+        v = self.v_proj(y)
+
+        # Split each number of heads
